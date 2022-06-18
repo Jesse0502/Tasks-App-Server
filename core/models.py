ce820795c9970a9c551ef8f031f9f7329c21de9a
Jesse0502 <jas0502singh@gmail.com>
2022-06-18 10:11:00 +0530

    resolved django secret key

--- core/models.py
+++ core/models.py
@@ -1,12 +1,6 @@
 from django.db import models
 
 
-# class SubTasks(models.Model):
-#     id = models.AutoField(primary_key=True)
-#     name = models.CharField(max_length=200)
-#     status = models.BooleanField(default=False)
-
-
 class Todo(models.Model):
     id = models.CharField(primary_key=True, max_length=100)
     name = models.CharField(max_length=200)
